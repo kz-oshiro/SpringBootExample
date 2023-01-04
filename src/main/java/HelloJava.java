a8d5606748ed731113b477b533b81e968effa234
Kazuma Oshiro <kazuma.oshiro.jp@gmail.com>
2023-01-04 15:27:47 +0900

    test commit

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -3,7 +3,7 @@ public class HelloJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub　
-		System.out.println("HelloJava");
+		System.out.println("Hello, Maven!");
 	}
 
 }
